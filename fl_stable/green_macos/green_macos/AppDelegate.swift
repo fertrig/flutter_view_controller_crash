2c26334efbdb6f98b1994a478d4147b0d31ee9e5
Fernando Trigoso <ftrigoso@dropsource.com>
2023-04-13 18:54:49 -0400

    issue reproducible

---
+++ fl_stable/green_macos/green_macos/AppDelegate.swift
@@ -0,0 +1,42 @@
+import Cocoa
+
+import FlutterMacOS
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        _launch_1()
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+    func _launch_1() {
+        let bundleUrl = URL(fileURLWithPath: "/Users/fertrig/development/scratch/macos_issue/fl_stable/bundle")
+        let bundle = Bundle.init(path: bundleUrl.path)
+        let project = FlutterDartProject.init(precompiledDartBundle: bundle)
+        let controller = FlutterViewController.init(project: project)
+        let window = NSWindow()
+        window.contentViewController = controller
+        let windowController = NSWindowController()
+        windowController.contentViewController = window.contentViewController
+        
+        window.setContentSize(NSSize(width: 300, height: 400))
+        window.setFrameOrigin(NSPoint(x: 100.0, y: 100.0))
+        window.title = "fish"
+        
+        windowController.window = window
+        windowController.showWindow(self)
+    }
+
+}
+
---
+++ fl_stable/green_macos/green_macos/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  green_macos
+//
+//  Created by Fernando Trigoso on 4/13/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
